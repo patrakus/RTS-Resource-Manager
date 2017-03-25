64f7744a16b4e3e3076cb17101a942eb82b5c035
Patryk <patrakus@interia.pl>
2017-03-25 23:18:53 +0100

    First commit

---
+++ Resource RTS/Resource.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine.UI;
+
+
+class Resource
+{
+    int _count;
+    Image _icon;
+    string _hint;
+    int _ID;
+
+    private static int IDCounter = 0;
+
+    public Resource(int count, Image icon, string hint)
+    {
+        _count = count;
+        _icon = icon;
+        _hint = hint;
+        _ID = ++IDCounter;
+    }
+
+    ~Resource()
+    {
+        --IDCounter;
+    }
+
+    public int Count
+    {
+        get
+        {
+            return _count;
+        }
+
+        set
+        {
+            _count = value;
+        }
+    }
+
+    public string Hint
+    {
+        get
+        {
+            return _hint;
+        }
+    }
+
+    public int ID
+    {
+        get
+        {
+            return _ID;
+        }
+    }
+}
---
+++ Resource RTS/ResourceManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour {
+
+    List<Resource> resourcesList = new List<Resource>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void IncrementResource( int amount, int resourceID)
+    {
+        throw new System.Exception();
+    }
+
+    void DecrementResource(int amount, int resourceID)
+    {
+        throw new System.Exception();
+    }
+
+    void AddResource(Resource newResource)
+    {
+        throw new System.Exception();
+    }
+
+    void RemoweResource(Resource Resource)
+    {
+        throw new System.Exception();
+    }
+
+    Resource GetResource(int ID) /// TODO: dorobić test dla tej metody
+    {
+        foreach (var item in resourcesList)
+        {
+            if (item.ID == ID)
+                return item;
+        }
+
+        return null;
+    }
+}
